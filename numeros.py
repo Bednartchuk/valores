739be3185e48763a89f61d32caa02b8caddeb86a
unknown <gustavohenriquebs7@gmail.com>
2023-03-06 21:43:17 -0300

    valores

---
+++ numeros.py
@@ -0,0 +1,8 @@
+valor1 = float(input("Insira o primeiro valor: "))
+
+valor2 = float(input("Insira o segundo valor: "))
+
+if valor1 > valor2:
+    print("O maior valor é:", valor1)
+else:
+    print("O maior valor é:", valor2)
